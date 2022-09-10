5501ae0e01d99288e89bdc589018ce1d6dd7fca0
xamanDawud <ahmeddaudking100@gmail.com>
2022-09-10 10:09:27 +0600

    todo done

--- js/script.js
+++ js/script.js
@@ -12,4 +12,25 @@ const clickHandler = () => {
         let todoList = [...haveTodo, { title: inputValue }];
         localStorage.setItem("Todo", JSON.stringify(todoList));
     }
+    render();
 };
+
+const render = () => {
+    let ul = getElementText("todo-list");
+    ul.innerHTML = "";
+    let todo = JSON.parse(localStorage.getItem("Todo"));
+    todo.forEach((item) => {
+        let li = document.createElement("li");
+        li.innerText = item.title;
+        li.classList.add("py-3");
+        ul.appendChild(li);
+    });
+    // clearItems();
+};
+
+const clearItems = () => {
+    localStorage.removeItem("Todo");
+    render();
+};
+
+render();
